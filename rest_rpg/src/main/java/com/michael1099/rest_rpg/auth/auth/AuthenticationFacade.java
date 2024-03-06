942674302f948e748a50058eab33b3e80b96d053
Michał Sulima <michal.sulima1999@gmail.com>
2024-03-06 20:47:57 +0100

    Tydzień 4, Facade

--- rest_rpg/src/main/java/com/michael1099/rest_rpg/auth/auth/AuthenticationFacade.java
+++ rest_rpg/src/main/java/com/michael1099/rest_rpg/auth/auth/AuthenticationFacade.java
@@ -9,6 +9,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
+// Tydzień 4, Facade
+// AuthenticationFacade jest fasadą dla operacji związanych z uwierzytelnianiem
+// Dzięki temu wzorzecowi fasady pozostała część aplikacji może korzystać z prostego interfejsu IAuthenticationFacade,
+// nie martwiąc się o szczegóły implementacyjne związane z uwierzytelnianiem.
+// Ta abstrakcja ułatwia także późniejsze zmiany w implementacji uwierzytelniania bez konieczności zmiany kodu w wielu miejscach.
 @Service
 public class AuthenticationFacade implements IAuthenticationFacade {
 
@@ -24,3 +29,4 @@ public class AuthenticationFacade implements IAuthenticationFacade {
         }
     }
 }
+// Koniec Tydzień 4, Facade
