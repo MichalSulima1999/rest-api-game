b0d6caf0aca1a5b59394b05c282bb5512be0fb81
Michal.Sulima1999 <michal.sulima1999@gmail.com>
2024-03-22 21:42:53 +0100

    Tydzień 6 - visitor - opis

--- rest_rpg/src/main/java/com/michael1099/rest_rpg/report/ReportVisitor.java
+++ rest_rpg/src/main/java/com/michael1099/rest_rpg/report/ReportVisitor.java
@@ -5,6 +5,12 @@ import com.michael1099.rest_rpg.equipment.Equipment;
 import com.michael1099.rest_rpg.statistics.Statistics;
 import org.openapitools.model.ReportResponse;
 
+// Tydzień 6 - visitor
+// Stworzony został interfejs ReportVisitor, który posiada definicję przeciążonej metody visit
+// Metoda może przyjąć Character, Statistics, Equipment. Interfejs jest zaimplementowany w ReportGenerator, gdzie znajduje się logika.
+// Interfejs Reportable ma metodę accept, która przyjmuje visitora. Ten interfejs jest implementowany przez Character, Statistics, Equipment.
+// W tych klasach wywoływana jest metoda visit z obiektem, z którego jest wywoływana.
+// W tym wypadku visitor pozwala na generowanie raportów poszczególnych obiektów.
 public interface ReportVisitor {
 
     ReportResponse visit(Character character);
@@ -13,3 +19,4 @@ public interface ReportVisitor {
 
     ReportResponse visit(Equipment equipment);
 }
+// Koniec Tydzień 6 - visitor
